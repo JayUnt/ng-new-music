9453abe93fbad9853261997416c395f61bd96fd5
Jason Unterman <junterman@definedlogic.com>
2021-02-17 11:51:41 -0500

    deployed and added todo

---
+++ TODO.md
@@ -0,0 +1,5 @@
+- Import albums
+- Form validation:
+  - rating: 1 - 10
+- Authentication
+- Associate ratings with user
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC0KA_hGXkIIBQQjO1SBGsJVgHX9toNwBA",
+    authDomain: "ng-new-music.firebaseapp.com",
+    projectId: "ng-new-music",
+    storageBucket: "ng-new-music.appspot.com",
+    messagingSenderId: "1005358600275",
+    appId: "1:1005358600275:web:0815d04d972e78dd8176a6",
+    measurementId: "G-678TELQ74V"
+  },
 };
